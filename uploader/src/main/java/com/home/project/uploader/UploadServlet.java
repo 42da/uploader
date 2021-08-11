1ab0817ec6a76f2941e2b2bc7a3f615547c3cd86
gipyeong <sb02096@naver.com>
2021-08-11 18:08:21 +0900

    third commit
     - where is blob object
     - json -> just form data

--- uploader/src/main/java/com/home/project/uploader/UploadServlet.java
+++ uploader/src/main/java/com/home/project/uploader/UploadServlet.java
@@ -18,6 +18,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.fileupload.MultipartStream;
 /*
  * Servlet implementation class UploadServlet
  */
@@ -46,76 +47,178 @@ public class UploadServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("post");
+		System.out.println("\npost");
 		request.setCharacterEncoding("utf-8");
 		String saveDirectory = "D:\\upload_path\\";
+		String path = "";
 		String tempDirectory = "D:\\temp\\";
-		System.out.println(saveDirectory);
+		
 		int maxSize = 1024 * 1024 * 5;
 		String encoding = "UTF-8";
-		
-		System.out.println(request.getHeader("header"));
-		
+
 //		MultipartRequest multi = new MultipartRequest(request, tempDirectory, maxSize, encoding, new DefaultFileRenamePolicy());
 		MultipartRequest multi = new MultipartRequest(request, tempDirectory, maxSize, encoding);
-		String fileInfo = multi.getParameter("fileInfo0");
+
+//		Enumeration files = multi.getFileNames();
+//		String item = (String) files.nextElement();
+////		String ofileName = multi.getOriginalFileName(item);
+//		String blob = multi.getFilesystemName(item);
 		
+		int start = Integer.parseInt(multi.getParameter("start"));
+		boolean divUpload = Boolean.parseBoolean(multi.getParameter("divUpload"));
+		String ofileName = multi.getParameter("originalName");
+		String guid = multi.getParameter("guid");
+		boolean first = Boolean.parseBoolean(multi.getParameter("first"));
+		boolean last = Boolean.parseBoolean(multi.getParameter("last"));
+		int fullSize = Integer.parseInt(multi.getParameter("fullSize"));
+		String name = multi.getParameter("name");
 		
-		try {
-			JSONParser parser = new JSONParser();
-			Object obj = parser.parse(fileInfo);
-			JSONObject jsonObj = (JSONObject) obj;
-			boolean divUpload = "1".equals(jsonObj.get("divUpload"));
+		System.out.println(start);
+		System.out.println(divUpload);
+		System.out.println(ofileName);
+		System.out.println(guid);
+		System.out.println(first);
+		System.out.println(last);
+		System.out.println(fullSize);
+		System.out.println(name);
+//		int size = ((Long) jsonObj.get("size")).intValue();
+//		String guid = (String) jsonObj.get("GUID");
+//		boolean divUpload = (Boolean) jsonObj.get("divUpload"); 
+		
+//		System.out.print("fileInfo : ");
+//		System.out.println(fileInfo);
+		
+		/* json parsing try 밖에서 쓰는 방법*/
+//		JSONObject jsonObj = null;
+		
+//		try {
+//			JSONParser parser = new JSONParser();
+//			Object obj = parser.parse(fileInfo);
+//			jsonObj = (JSONObject) obj;	
+//		} catch(Exception e) {
+//			
+//		}
+		/*------------------------------*/
+//		boolean first = (Boolean) jsonObj.get("first");
+//		boolean last = (Boolean) jsonObj.get("last");
+//		String ofileName = (String) jsonObj.get("originalName");
+//		String name = (String) jsonObj.get("name");
+//		int size = ((Long) jsonObj.get("size")).intValue();
+//		String guid = (String) jsonObj.get("GUID");
+//		boolean divUpload = (Boolean) jsonObj.get("divUpload"); 
+		
+		if (first) {
+			System.out.println("first");	
 			
-			Enumeration files = multi.getFileNames();
-			String item = (String) files.nextElement();
-			String ofileName = multi.getOriginalFileName(item);
-			if (!divUpload) {
-//				String fileName = multi.getFilesystemName(item);
-				
-				FileInputStream temp_file = new FileInputStream(tempDirectory + ofileName);
-				FileOutputStream main_file = new FileOutputStream(saveDirectory + ofileName);
-				int i = 0;
-				while((i = temp_file.read()) != -1) {
-					main_file.write(i);
-				}
-				temp_file.close();
-				main_file.close();
-			} else {
-				boolean first = "1".equals(jsonObj.get("first"));
-				boolean last = "1".equals(jsonObj.get("last"));
-				if (first) {
-					int size = ((Long) jsonObj.get("size")).intValue();
-					String guid = (String) jsonObj.get("GUID");
-					
-					RandomAccessFile guid_file = new RandomAccessFile(tempDirectory + guid + ".txt", "rw");
-					guid_file.writeChars(guid);
-					
-					String blob = multi.getFilesystemName(item);
-					FileInputStream chunk_file = new FileInputStream(tempDirectory + blob);
-					
-					RandomAccessFile main_file = new RandomAccessFile(saveDirectory + ofileName, "rw");
-					main_file.setLength(size);
-					
-					guid_file.close();
-					chunk_file.close();
-					main_file.close();
-					
-					response.setContentType("text/plain");
-					response.setCharacterEncoding("utf8");
-					PrintWriter out = response.getWriter();
-					out.println("uploading");
-					
-				} else if (last) {
-					
-				} else {
-					
-				}
-				
+			RandomAccessFile main_file = new RandomAccessFile(saveDirectory + ofileName, "rw");
+			main_file.setLength(fullSize);
+			main_file.close();
+			
+			File file = new File(tempDirectory + guid + ".txt");
+			FileOutputStream tmp_path = new FileOutputStream(file);
+			tmp_path.write(saveDirectory.getBytes());
+			tmp_path.close();
+
+			path = saveDirectory;
+
+		} else {
+			FileInputStream tmp_path = new FileInputStream(tempDirectory + guid + ".txt");
+			int i = 0;
+			while((i = tmp_path.read()) != -1) {
+				path = Integer.toString(i);
 			}
-		} catch (Exception e) {
-			System.out.println("fail");
+			tmp_path.close();
 		}
+		System.out.println("first complete");
+		// write
+		RandomAccessFile main_file = new RandomAccessFile(path + ofileName, "rw");
+		main_file.seek(start);
+		
+		FileInputStream chunk_file = new FileInputStream(tempDirectory + name);			// where is blob file
+		int data = 0;
+		while((data = chunk_file.read()) != -1) {
+			main_file.write(data);
+		}
+		main_file.close();
+		chunk_file.close();
+		
+		System.out.println("write complete");
+		// last chunk?
+		if (divUpload && last) {
+			File tmp_path = new File(tempDirectory + guid + ".txt");
+			tmp_path.delete();
+			response.setContentType("text/plain");
+			response.setCharacterEncoding("utf8");
+			PrintWriter out = response.getWriter();
+			out.println(path);
+		} else if (!divUpload) {
+			File tmp_path = new File(tempDirectory + guid + ".txt");
+			tmp_path.delete();
+			response.setContentType("text/plain");
+			response.setCharacterEncoding("utf8");
+			PrintWriter out = response.getWriter();
+			out.println(path);
+		}
+		
+		
+//		boolean divUpload = "1".equals(jsonObj.get("divUpload")); // jsonObj.get(divUpload): string, jsonObj.get(first): boolean => ??
+		
+//		Enumeration files = multi.getFileNames();
+//		String item = (String) files.nextElement();
+//		String ofileName = multi.getOriginalFileName(item);
+
+
+//			if (!divUpload) {
+//				
+//				String fileName = multi.getFilesystemName(item);
+//				
+//				FileInputStream temp_file = new FileInputStream(tempDirectory + ofileName);
+//				FileOutputStream main_file = new FileOutputStream(saveDirectory + ofileName);
+//				int i = 0;
+//				while((i = temp_file.read()) != -1) {
+//					main_file.write(i);
+//				}
+//				temp_file.close();
+//				main_file.close();
+//				response.setContentType("text/plain");
+//				response.setCharacterEncoding("utf8");
+//				PrintWriter out = response.getWriter();
+//				out.println(saveDirectory);
+//				System.out.println("!divupload");
+//				
+//			} else {
+//				boolean first = "1".equals(jsonObj.get("first"));
+//				boolean last = "1".equals(jsonObj.get("last"));
+//				if (first) {
+//					int size = ((Long) jsonObj.get("size")).intValue();
+//					String guid = (String) jsonObj.get("GUID");
+//					
+//					RandomAccessFile guid_file = new RandomAccessFile(tempDirectory + guid + ".txt", "rw");
+//					guid_file.writeChars(guid);
+//					
+//					String blob = multi.getFilesystemName(item);
+//					FileInputStream chunk_file = new FileInputStream(tempDirectory + blob);
+//					
+//					RandomAccessFile main_file = new RandomAccessFile(saveDirectory + ofileName, "rw");
+//					main_file.setLength(size);
+//					
+//					guid_file.close();
+//					chunk_file.close();
+//					main_file.close();
+//					
+//					response.setContentType("text/plain");
+//					response.setCharacterEncoding("utf8");
+//					PrintWriter out = response.getWriter();
+//					out.println("uploading");
+					
+//				} else if (last) {
+//					
+//				} else {
+//					
+//				}
+//				
+//			}
+		
 		
 //		try {
 //			JSONParser parser = new JSONParser();
@@ -152,11 +255,6 @@ public class UploadServlet extends HttpServlet {
 //			System.out.println("fail");
 //		}
 		
-		System.out.println("success");
-		
-		
-		
-		
 		
 //		byte[] byteArray = FileUtils.readFileToByteArray(new File(tempDirectory + fileName));
 //		FileUtils.writeByteArrayToFile(new File(saveDirectory + fileName), byteArray, true); // fileUtil 이어 쓰기 (마지막 true)
@@ -174,10 +272,10 @@ public class UploadServlet extends HttpServlet {
 //			byte[] byteArray = FileUtils.readFileToByteArray(new File(tempDirectory + fileName));
 //			FileUtils.writeByteArrayToFile(new File(saveDirectory + fileName), byteArray, true); // fileUtil 이어 쓰기 (마지막 true)
 //		}
-		response.setContentType("text/plain");
-		response.setCharacterEncoding("utf8");
-		PrintWriter out = response.getWriter();
-		out.println(saveDirectory);
+//		response.setContentType("text/plain");
+//		response.setCharacterEncoding("utf8");
+//		PrintWriter out = response.getWriter();
+//		out.println(saveDirectory);
 		
 
 	    
--- uploader/src/main/webapp/uploader.js
+++ uploader/src/main/webapp/uploader.js
@@ -12,21 +12,80 @@
 		return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
 			s4() + '-' + s4() + s4() + s4();
 	}
-	function upload() {
-		var xhr = new XMLHttpRequest();
-		xhr.open('POST', '/uploader/UploadServlet');
-		if (start > end) return;
-		xhr.onreadystatechange = function () {
-			if (xhr.status === 200) { // In local files, status is 0 upon success in Mozilla Firefox
-//				if () last index
-				upload()
-			}
-		}
+	function upload(start, end, chunkSize, divUpload, originalName, guid, first, last, fileIndex, fullSize, name) {
 		
+		var xhr = new XMLHttpRequest();
+		
+		xhr.open('POST', '/uploader/UploadServlet');
+		if (divUpload) fileList[fileIndex] = fileList[fileIndex]['obj'].slice(start, end);
+		console.log(first, fileList[fileIndex]);
+		formData.set('file', fileList[fileIndex]['obj']);
+		formData.set('start', start);
+		formData.set('divUpload', divUpload);
+		formData.set('originalName', originalName);
+		formData.set('guid', guid);
+		formData.set('first', first);
+		formData.set('last', last);
+		formData.set('fullSize', fullSize);
+		formData.set('name', name)
+//		if (first) {
+//			formData.set('first', first);
+////			formData.set('fileInfo', JSON.stringify(fileList[fileIndex]));
+////			xhr.setRequestHeader("first", true);
+//		} else if (!first && last){
+//			formData.set('last', last);
+////			xhr.setRequestHeader("first", false);
+//		}
+		
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === xhr.DONE) {
+				if (xhr.status === 0 || (xhr.status >= 200 && xhr.status < 400)) { // In local files, status is 0 upon success in Mozilla Firefox
+//					formData.delete('file');
+//					formData.delete('fileInfo');
+//					first = false;
+					if (!divUpload || last) {
+						var loaded_list = document.createElement("ul");
+						loaded_list.className = "completed_list";
+						var loaded_file = document.createElement("li");
+						loaded_file.className = "completed_file";
+						loaded_file.innerText = fileList[fileIndex]['originalName'] + " 업로드 완료";
+						loaded_list.appendChild(loaded_file);
+						loaded_list.style.width = "768px";
+						loaded_list.style.textAlign = "right";
+						document.body.appendChild(loaded_list);
+						
+						console.log(start, end, chunkSize, divUpload, first, last, fileIndex, fullSize);
+						console.log(fileList);
+						console.log(xhr.responseText);
+
+						fileList[fileIndex]['path'] = xhr.responseText + fileList[fileIndex]['originalName'];
+
+						if (++fileIndex == fileList.length) return; 
+						else {
+							start = 0;
+							end = chunkSize;
+							last = false;
+						}
+
+					} else {
+						start = end;
+						end = start + chunkSize;
+						first = false;
+						if (end >= fullSize) {
+							end = fullSize;
+							last = true;
+						}
+					}
+					upload(start, end, chunkSize, fileList[fileIndex]['divUpload'], fileList[fileIndex]['first'], fileList[fileIndex]['last'], fileIndex, fullSize, fileList[fileIndex]['name']);
+				}
+			}
+			
+		}
+		xhr.send(formData);
 	}
 	var fileList = [];
 	var formData = new FormData();
-	var chunkSize = 10 * 10 * 2;
+	var chunkSize = 1024 * 1024 * 5;
 	var start = 0;
 	var end = chunkSize;
 	window.onload = function() {
@@ -45,32 +104,20 @@
 						input.addEventListener("change", function() {
 							
 							var file = input.files;
+							console.log(file);
 							var ol = document.getElementById("file_list");
 							for (var index = 0; index < file.length; index++) {
-								if (file[index]['size'] < chunkSize) {
-									fileList.push(
+								fileList.push(
 										{
+//											obj: file[index]['size'] > chunkSize ? file[index].slice(start, chunkSize) : file[index],
 											obj: file[index],
 											fileIndex: index,
+											size: file[index]['size'],
+											originalName: file[index]['name'],
 											name: file[index]['name'].substring(0, file[index]['name'].lastIndexOf('.')),
 											extension: '.' + file[index]['name'].split('.').pop(),
-											size: file[index]['size'],
-											divUpload: false,
-											path: ''
-										}
-									);
-								} else {
-									console.log(file[index].slice(start, chunkSize)['size'], 'file slice');
-									fileList.push(
-										
-										{
-											obj: file[index].slice(start, chunkSize),
-											fileIndex: index,
-											name: file[index]['name'].substring(0, file[index]['name'].lastIndexOf('.')),
-											extension: '.' + file[index]['name'].split('.').pop(),
-											size: file[index]['size'],
 											chunksize: file[index].slice(start, chunkSize)['size'],
-											divUpload: true,
+											divUpload: file[index]['size'] > chunkSize ? true : false,
 											lastModified: file[index]['lastModified'],
 											GUID: guid(),
 											path: '',
@@ -78,10 +125,6 @@
 											last: false
 										}
 									);
-								}
-								
-								formData.append('file' + index, fileList[index]['obj']);
-								formData.append('fileInfo' + index, JSON.stringify(fileList[index]));
 								
 								var li = document.createElement("li");
 								var ul = document.createElement("ul");
@@ -121,7 +164,7 @@
 								ul.appendChild(fsize);
 								li.appendChild(ul);
 								ol.appendChild(li);
-								//debugger;
+								
 							}
 							
 							list.style.height = String(file.length * 21) + "px";
@@ -132,32 +175,8 @@
 						});
 						break;
 					case "submit":
-						
-						var xhr = new XMLHttpRequest();
-						
-						xhr.addEventListener("load", function () {
-							var loaded_list = document.createElement("ul");
-							var loaded_file = document.createElement("li");
-							loaded_file.innerText = this.responseText + " 업로드 완료";
-							loaded_list.appendChild(loaded_file);
-							loaded_list.style.width = "768px";
-							loaded_list.style.textAlign = "right";
-							for (var j = 0; j < fileList.length; j++) {
-								fileList[j]['path'] = this.responseText;
-							}
-							console.log("file loaded");
-							
-							console.log(this.status);
-							
-							document.body.appendChild(loaded_list);
-							
-							
-						});
-						
-						xhr.open('POST', '/uploader/UploadServlet');
-						xhr.setRequestHeader("header", "request header");
-						xhr.send(formData);
-						
+						upload(start, end, chunkSize, fileList[0]['divUpload'], fileList[0]['originalName'], fileList[0]['GUID'],
+										fileList[0]['first'], fileList[0]['last'], 0, fileList[0]['size'], fileList[0]['name']);
 						break;
 					case "delete":
 						break;
